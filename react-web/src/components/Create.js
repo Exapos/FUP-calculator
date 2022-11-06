9ca8cddbe948fdeb0ff13fc4673d5f4f729cecad
Tomáš <exapos@seznam.cz>
2022-11-06 15:57:05 +0100

    WIP - fixed placeholder color

--- react-web/src/components/Create.js
+++ react-web/src/components/Create.js
@@ -9,17 +9,17 @@ function Create({label, id,idText, option}) {
                 <input type="text" placeholder={label} id={idText}/>
             </label>
             <select id={option}>
-                <option value="Bytes">Bytes[B/s]</option>
-                <option value="Kilobytes">Kilobytes[KB/s]</option>
-                <option value="Megabytes">Megabytes[MG/s]</option>
-                <option value="Gigabytes">Gigabytes[GB/s]</option>
-                <option value="Terabytes">Terabytes[TB/s]</option>
+                <option value="Bytes">Bytes [B/s]</option>
+                <option value="Kilobytes">Kilobytes [KB/s]</option>
+                <option value="Megabytes">Megabytes [MG/s]</option>
+                <option value="Gigabytes">Gigabytes [GB/s]</option>
+                <option value="Terabytes">Terabytes [TB/s]</option>
                 <option value=""></option>
-                <option value="Bits">Bits[b/s]</option>
-                <option value="Kilobits">Kilobits[Kb/s]</option>
-                <option value="Megabits">Megabits[Mb/s]</option>
-                <option value="Gigabits">Gigabits[Gb/s]</option>
-                <option value="Terabits">Terabits[Tb/s]</option>
+                <option value="Bits">Bits [b/s]</option>
+                <option value="Kilobits">Kilobits [Kb/s]</option>
+                <option value="Megabits">Megabits [Mb/s]</option>
+                <option value="Gigabits">Gigabits [Gb/s]</option>
+                <option value="Terabits">Terabits [Tb/s]</option>
             </select>
         </form>
     </div>
